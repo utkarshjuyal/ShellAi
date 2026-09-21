6231758c00bfa285556773f3512d0b772ba3246e
gc-MayankPun <punmayank35@gmail.com>
2026-09-21 17:15:56 +0530

    Added download extension button

---
+++ Frontend/src/app/components/DownloadExtension.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { FaDownload } from "react-icons/fa";
+
+const DownloadExtension = () => {
+  return (
+    <a
+      href="#"
+      target="_blank"
+      className="lp-btn lp-btn--download"
+    >
+      <FaDownload /> Download Extension
+    </a>
+  );
+};
+
+export default DownloadExtension;
